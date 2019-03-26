eea6998376f9bb07164b93899e39689f9c2daf41
tronghuan98hd <30307373+tronghuan98hd@users.noreply.github.com>
2019-03-26 23:32:30 +0700

    first commit

---
+++ Webpkdt/Webpkdt/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webpkdt.Models.product;
+
+namespace Webpkdt.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(product.get_list());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(product Pr)
+        {
+            return View();
+        }
+    }
+}
---
+++ Webpkdt/Webpkdt/Models/product/product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webpkdt.Models.product
+{
+    public class product
+    {
+        private int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public static List<product> list_product = new List<product>()
+        {
+            new product() {id = 1, name = "Tai nghe khong day", price = 200000},
+            new product() {id = 2, name = "Op lung", price = 300000},
+            new product() {id = 3, name = "Gay chup anh tu suong", price = 400000},
+            new product() {id = 4, name = "Cap sac iphone", price = 500000},
+            new product() {id = 4, name = "Sac du phong", price = 100000},
+        };
+        public static List<product> get_list()
+        {
+            return list_product;
+        }
+        public void Add(product pr)
+        {
+            list_product.Add(pr);
+        }
+    }
+}
