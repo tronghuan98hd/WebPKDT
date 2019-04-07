ef138ad4476c3800f060b8135d93b8e5ac4e728e
tronghuan98hd <30307373+tronghuan98hd@users.noreply.github.com>
2019-04-07 09:58:54 +0700

    add theme
    thêm giao diện cơ bản choh trang quản trị

---
+++ Webpkdt/Webpkdt/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webpkdt.Models.user;
+
+namespace Webpkdt.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(user.get_list());
+        }
+
+        //Tao moi user
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(user us)
+        {
+            user.list_user.Add(us);
+            return RedirectToAction("Index");
+        }
+
+        //Sua thong tin user
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var us = user.list_user.SingleOrDefault(x => x.id == id);
+            return View(us);
+        }
+        [HttpPost]
+        public ActionResult Edit(user us)
+        {
+            var findus = user.list_user.SingleOrDefault(x => x.id == us.id);
+            if(findus != null)
+            {
+                findus.name = us.name;
+                findus.adress = us.adress;
+                findus.phone = us.phone;
+                findus.email = us.email;
+            }
+            return RedirectToAction("Index");
+        }
+
+        //Xoa thong tin user
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var us = user.list_user.SingleOrDefault(x => x.id == id);
+            return View(us);
+        }
+        [HttpPost]
+        public ActionResult Delete(user us)
+        {
+            var findus = user.list_user.SingleOrDefault(x => x.id == us.id);
+            user.list_user.Remove(findus);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Webpkdt/Webpkdt/Models/user/user.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webpkdt.Models.user
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string adress { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public static List<user> list_user = new List<user>()
+        {
+            new user() {id = 1, name = "Nguyen Van A", adress="Ha Noi", phone="0123", email="abc@yahoo.com"},
+            new user() {id = 2, name = "Nguyen Van B", adress="Hai Duong", phone="0389", email="xyz@yahoo.com"},
+            new user() {id = 3, name = "Nguyen Van C", adress="Thai Binh", phone="0357", email="mnp@gmail.com"},
+            new user() {id = 4, name = "Nguyen Van D", adress="Quang Ninh", phone="0983", email="example@icloud.com"},
+            new user() {id = 5, name = "Nguyen Van E", adress="Hai Phong", phone="0977", email="cbc@gmail.com"},
+        };
+        public static List<user> get_list()
+        {
+            return list_user;
+        }
+    }
+}
